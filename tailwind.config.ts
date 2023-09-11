89933e51f901f2a4a197b18742ed7bb6421ffb90
Shantanu <shantanuwable2003@gmail.com>
2023-09-11 13:07:09 +0530

    Added font and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,16 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'navy':'#27005D',
+      'purple':'#9400FF',
+      'skyblue':'#AED2FF',
+      'lightblue':'#E4F1FF'
+    },
     extend: {
+      fontFamily: {
+        'sfmedium':['var(--font-sfmedium)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
