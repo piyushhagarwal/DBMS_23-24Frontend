586cb9d036769e645ec44a0369106234ce384fa4
Shantanu <shantanuwable2003@gmail.com>
2023-09-11 13:07:47 +0530

    Added font variable for sfmedium

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import localFont from '@next/font/local'
 import type { AppProps } from 'next/app'
+
+const sfmedium = localFont({
+  src: [
+    {
+      path: '../public/fonts/SF-Pro-Display-Medium.otf',
+      weight: '400'
+    }
+  ],
+  variable: '--font-sfmedium'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
