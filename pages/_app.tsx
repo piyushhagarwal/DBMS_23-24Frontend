f06b054e5c6d8c0cd6aa972845b41add5e466f4d
Shantanu <shantanuwable2003@gmail.com>
2023-09-12 10:45:52 +0530

    Wrapped _app.tsx in NextUIProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
-import '@/styles/globals.css'
-import localFont from '@next/font/local'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import localfont from "next/font/local";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
 
-const sfmedium = localFont({
-  src: [
-    {
-      path: '../public/fonts/SF-Pro-Display-Medium.otf',
-      weight: '400'
-    }
-  ],
-  variable: '--font-sfmedium'
-})
+import RootLayout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <NextUIProvider>
+            <RootLayout>
+                <Component {...pageProps} />
+            </RootLayout>
+        </NextUIProvider>
+    );
 }
