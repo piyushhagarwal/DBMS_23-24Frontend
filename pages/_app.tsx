806f564a713b027afe6720c455513a13e56cde33
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-09-12 12:09:53 +0530

    Made few changes in cards component and added footer to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-
 import "@/styles/globals.css";
 import localfont from "next/font/local";
 import type { AppProps } from "next/app";
@@ -6,14 +5,16 @@ import { NextUIProvider } from "@nextui-org/react";
 import Navbar from "@/components/Navbar";
 
 import RootLayout from "@/components/layout/layout";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <NextUIProvider>
-            <RootLayout>
-                <Navbar />
-                <Component {...pageProps} />
-            </RootLayout>
-        </NextUIProvider>
-    );
+  return (
+    <NextUIProvider>
+      <RootLayout>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </RootLayout>
+    </NextUIProvider>
+  );
 }
