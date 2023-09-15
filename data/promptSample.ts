03674d17713f93b88c1b7817cbb42a76065bfa18
Shantanu <shantanuwable2003@gmail.com>
2023-09-15 14:04:22 +0530

    Sample prompt and its corresponding response

---
+++ data/promptSample.ts
@@ -0,0 +1,24 @@
+export const samplePrompt: string = "भूमि संपत्ति के संबंध में सम्पत्ति कानून क्या कहता है?"
+
+export const sampleResponse: string = `Sure, I'd be happy to help you with your query.
+
+The term "भूमि संपत्ति" is a term commonly used in India to refer to the land ownership rights of rural landless laborers, who have been traditionally denied access to land ownership due to various historical, social, and economic factors. In this context, the term "संबंध" refers to the legal framework that governs the relationship between the State and the landless laborers, and the term "कानून" refers to the laws and regulations that govern this relationship.
+
+To answer your question, the Conversion of Bhumi Rights to Proprietary Rights: A Step-by-Step Guide
+
+Step 1: Understanding the Legal Framework
+
+* The Bhumi Act, 1956: This act aims to protect the rights of rural landless laborers and provide them with legal ownership of the land they have been working on for generations.
+* The Intermediate and Final Certificates issued under the Bhumi Act: These certificates serve as proof of land ownership for landless laborers and are issued by the Government of India.
+* The Land Ceiling Act, 1972: This act aims to regulate the transfer of land ownership and prevent concentration of land holdings in a few hands.
+
+Step 2: Identifying the Relevant Laws and Regulations
+
+* The Right to Information Act, 2005: This act provides the right to information to citizens, which includes the right to access information about land ownership and transfer.
+* The Constitution of India: The Constitution guarantees the right to property and provides for the protection of the interests of the weaker sections of society.
+
+Step 3: Analyzing the Relevant Cases and Precedents
+
+* The case of Bhajan Lal vs. Union of India (1996): In this case, the Supreme Court of India held that the Bhumi Act, 1956 is a social welfare legislation and protects the occupancy rights of landless laborers.
+* The case of Suresh Kumar vs. Union of India (2005): In this case, the Supreme Court of India held that the right to land ownership is a fundamental right under Article 300
+`;
