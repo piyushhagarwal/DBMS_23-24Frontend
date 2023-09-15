cd4539c2415f2ed808b1ad5084e4ee8c44c57df9
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-09-15 16:10:21 +0530

    added more data

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,39 +4,39 @@ import { useState } from "react";
 import { samplePrompt, sampleResponse } from "@/data/promptSample";
 
 function Hero() {
-    const [output, setOutput] = useState(false);
+  const [output, setOutput] = useState(false);
 
-    return (
-        <div className="text-gray-600 body-font">
-            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                <div className="text-center lg:w-2/3 w-full">
-                    <h1 className="title-font text-8xl mb-4 pb-4 text-gray-900">
-                        Know Your Rights
-                    </h1>
-                    <div className="flex flex-row pb-2 space-x-5">
-                        <Input
-                            type=""
-                            variant="bordered"
-                            label="Prompt"
-                            className=""
-                            defaultValue={samplePrompt}
-                        />
-                        <button className="inline-flex text-white bg-purple-400 border-0 py-3 px-6 focus:outline-none hover:bg-purple-500 rounded text-lg">
-                            Submit
-                        </button>
-                        {/* <Button color="secondary" size="lg" radius="lg" >Button</Button> */}
-                    </div>
-                    <Textarea
-                        isReadOnly
-                        variant="underlined"
-                        labelPlacement="outside"
-                        defaultValue={sampleResponse}
-                        size="lg"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 pt-16 items-center justify-center flex-col">
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font text-8xl mb-4 pb-4 text-gray-900">
+            Know Your Rights
+          </h1>
+          <div className="flex flex-row pb-2 space-x-5">
+            <Input
+              type=""
+              variant="bordered"
+              label="Prompt"
+              className=""
+              defaultValue={samplePrompt}
+            />
+            <button className="inline-flex text-white bg-purple-400 border-0 py-3 px-6 focus:outline-none hover:bg-purple-500 rounded text-lg">
+              Submit
+            </button>
+            {/* <Button color="secondary" size="lg" radius="lg" >Button</Button> */}
+          </div>
+          <Textarea
+            isReadOnly
+            variant="underlined"
+            labelPlacement="outside"
+            defaultValue={sampleResponse}
+            size="lg"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- components/LawCards.tsx
+++ components/LawCards.tsx
@@ -17,9 +17,6 @@ function LawCards() {
             {sampleData.map(({ catName, catDesc, id }: catDetails) => (
               <div key={id} className="p-4 lg:w-1/3">
                 <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                    {catName}
-                  </h2>
                   <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
                     {catName}
                   </h1>
