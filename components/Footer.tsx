b3237a7b29df62c756352556001f882cc31f0954
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-09-15 15:30:45 +0530

    updated footer name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,7 +29,7 @@ function Footer() {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">LAWda Lassan</span>
+          <span className="ml-3 text-xl">Juris</span>
         </a>
       </div>
     </footer>
