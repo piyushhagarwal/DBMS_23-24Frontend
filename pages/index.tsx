dca80abc2f0f1cc064f345db3c40415afa7750f3
Shantanu <shantanuwable2003@gmail.com>
2023-09-18 13:09:49 +0530

    Removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,7 @@ import ChatButton from "@/components/Chatbot";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import LawCards from "@/components/LawCards";
-import Navbar from "@/components/Navbar";
 
-import { useContext, useEffect, useState } from "react";
-import { LanguageContext } from "@/context/LanguageContext";
-
-import { English } from "@/constants/Englishdata";
-import { Hindi } from "@/constants/hindidata";
-import { Language } from "@/enums/language";
 
 export default function Home() {
     return (
