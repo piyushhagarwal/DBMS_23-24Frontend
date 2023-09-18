34189eae9043ad1b580266ba2ca4035d66a8ba92
Shantanu <shantanuwable2003@gmail.com>
2023-09-18 13:09:38 +0530

    Uncommented law cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,19 @@ import Hero from "@/components/Hero";
 import LawCards from "@/components/LawCards";
 import Navbar from "@/components/Navbar";
 
+import { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "@/context/LanguageContext";
+
+import { English } from "@/constants/Englishdata";
+import { Hindi } from "@/constants/hindidata";
+import { Language } from "@/enums/language";
+
 export default function Home() {
-  return (
-    <div>
-      <Hero />
-      {/* <LawCards /> */}
-      <ChatButton />
-    </div>
-  );
+    return (
+        <div>
+            <Hero />
+            <LawCards />
+            <ChatButton />
+        </div>
+    );
 }
